d05a8cdc59e3ee8eaab51cadef1fb6d5dffa0aa8
Muehlehner Moritz <mmuehlehner@student.tgm.ac.at>
2016-02-24 18:20:46 +0100

    Some Formatting and Tests

--- Prepared Statements/Main.java
+++ Prepared Statements/Main.java
@@ -1,40 +1,41 @@
 import java.sql.*;
 
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args) throws Exception {
-		DBConnection dbcon;
-		Statements s;
-		CLI cliargs = new CLI(args);
-		String[] argsstring = cliargs.returnArgs();
-		Connection c;
-		dbcon = new DBConnection("dbconnection.properties");
-		// Wenn die Verbindung funktioniert (also nicht null zurück liefert)
-		// wird die Conncetion gespeichert
-		
-		if (dbcon.connect() != null) {
-			c = dbcon.connect();
-		} else { // Wenn nicht, wird versucht sich durch die Arguments mit der
-					// Datenbank zu verbinden
-			try {
-				dbcon = new DBConnection(argsstring);
-				c = dbcon.connect();
-			} catch (Exception ex) { // Wenn das auch nicht geht, wird der
-										// Benutzer durch eine Ausgabe
-										// informiert
-				System.err.println("Kein/ungültiges Properties File und keine/ungültige Arguments vorhanden!");
-				throw new Exception("Kein/ungültiges Properties File und keine/ungültige Arguments vorhanden!");
-			}
+    public static void main(String[] args) throws Exception
+    {
+	DBConnection dbcon;
+	Statements s;
+	CLI cliargs = new CLI(args);
+	String[] argsstring = cliargs.returnArgs();
+	Connection c;
+	dbcon = new DBConnection("dbconnection.properties");
+	// Wenn die Verbindung funktioniert (also nicht null zurück liefert) wird die Conncetion gespeichert
+	if (dbcon.connect() != null)
+	{
+	    c = dbcon.connect();
+	} else
+	{ // Wenn nicht, wird versucht sich durch die Arguments mit der Datenbank zu verbinden
 
-		}
-		s = new Statements(c);
-		Statement st = c.createStatement();
-		ResultSet rs = st.executeQuery("select * from number");
-		while (rs.next()) {
-			String tmp = rs.getString(1);
-			System.out.println(tmp);
-		}
+	    try
+	    {
+		dbcon = new DBConnection(argsstring);
+		c = dbcon.connect();
+	    } catch (Exception ex)
+	    { // Wenn das auch nicht geht, wird der Benutzer durch eine Ausgabe informiert
+		System.err.println("Kein/ungültiges Properties File und keine/ungültige Arguments vorhanden!");
+		throw new Exception("Kein/ungültiges Properties File und keine/ungültige Arguments vorhanden!");
+	    }
 
 	}
+	s = new Statements(c);
+	s.create();
+	s.read();
+	s.update();
+	s.read();
+	s.delete();
+
+    }
 
 }
